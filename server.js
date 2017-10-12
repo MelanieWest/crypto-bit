5d5b1e75b626e5577f09d7da8c9209d95987426c
Melanie West <dhmofan@gmail.com>
2017-10-11 22:53:25 -0400

    Modified server.js to generate graphs of data for each coin type.  For now it's populated with random data

--- server.js
+++ server.js
@@ -37,24 +37,147 @@ app.use('/', routes);
 app.listen(port);
 
 
+// Express and MySQL code should go here.
 app.get('/charts', (req, res) => {
-    console.log('app get');
-  connection.query('SELECT `act_val`,`exch_date` FROM `currency` WHERE `coin_name`="bitcoin"', (err, results) => {
-    res.render('index', {bitCoin: results});
-      var data =[
-        {
-          x: [results[0].exch_date, results[1].exch_date, results[2].exch_date, results[3].exch_date, results[4].exch_date, results[5].exch_date],
-          y: [results[0].act_val, results[1].act_val, results[2].act_val, results[3].act_val, results[4].act_val, results[5].act_val],
-          type: "scatter"
+    connection.query('SELECT `act_val`,`exch_date`,`predict_val`,`emot` FROM `currency` WHERE `coin_name`="bitcoin"', (err, results) => {
+     res.render('index', {bitCoin: results});
+  
+      var datax=[];
+      var data1y=[];
+      var data2y=[];
+  
+      for (var i = 0; i < results.length; i++) {
+  
+        //push data into arrays for graphing; modify values to be closer
+        // to 3800 (current value)
+  
+      datax.push(results[i].exch_date);
+      data1y.push(results[i].act_val*2 + 3500);
+      //change boolean false to -1;  then scale up values for graphing 
+      data2y.push(results[i].emot);
+      if (data2y[i] == 0){
+        data2y[i] = -200;
+      }else{
+        data2y[i] = 200;
+      }
+    }
+     
+     var trace1 = {
+       x: datax,
+       y: data1y,
+       name: "Bitcoin Values",
+       type: "scatter"
+     };
+     var trace2 = {
+       x: datax,
+       y: data2y,
+       name: "Predictions",
+       type: "bar"
+     };
+     var data = [trace1, trace2];
+     var graphOptions = {filename: "Bitcoin", fileopt: "overwrite"};
+     plotly.plot(data, graphOptions, function (err, msg) {
+         console.log(msg);
+     });
+  
+     //create two more tables using random data
+  
+     //create random data for coin and boolean values;
+     var testArrayLength = 7;
+     var newBool;
+     // assume Etherium is starting at a current value of 300;
+     var seedEthereum = 300;
+     var ether =[];
+     var boolE =[];
+     var newEthVal;
+  
+     //create random data for ethereum
+     for (var j=0; j<testArrayLength; j++){
+        if (j == 0){
+          newEthVal = seedEthereum*(1+((Math.floor(Math.random()*3)-1)*(.05)));         
+        }else{
+          newEthVal = ether[j-1]*(1+((Math.floor(Math.random()*3)-1)*(.05)));         
         }
-      ];
-      var graphOptions = {filename: "date-axes", fileopt: "overwrite"};
-      plotly.plot(data, graphOptions, function (err, msg) {
-          console.log(msg);
-      });
-          console.log(JSON.stringify(results));
-          console.log(results);
-   });
-        
-});
+        ether.push(newEthVal);
+        newBool = Math.floor(Math.random()*2);
+        boolE.push(newBool);
+        if(boolE[j]==0){
+          boolE[j]= -20;
+        }else{
+          boolE[j]= 20;
+        }
+     }
+     console.log('etherium: '+ether+boolE);
+  
+     var trace1 = {
+      x: datax,
+      y: ether,
+      name: "Ethereum Values",
+      type: "scatter"
+    };
+    var trace2 = {
+      x: datax,
+      y: boolE,
+      name: "Predictions",
+      type: "bar"
+    };
+    var data = [trace1, trace2];
+    var graphOptions = {filename: "Etherium", fileopt: "overwrite"};
+    plotly.plot(data, graphOptions, function (err, msg) {
+        console.log(msg);
+    });
+  
+  
+  
+  
+  
+  
+     // assume litecoin is starting at a current value of 50;
+     var newLiteVal;
+     var seedLiteCoin = 50;
+     var boolL =[];
+     var lite =[];
+     
+     //create random data
+     for (var k=0; k<testArrayLength; k++){
+      if (k == 0){
+        newLiteVal = seedLiteCoin*(1+((Math.floor(Math.random()*3)-1)*(.05)));         
+      }else{
+        newLiteVal = lite[k-1]*(1+((Math.floor(Math.random()*3)-1)*(.05)));         
+      }
+      lite.push(newLiteVal);
+      newBool = Math.floor(Math.random()*2);
+      boolL.push(newBool);
+      if(boolL[k]==0){
+        boolL[k]= -20;
+      }else{
+        boolL[k]= 20;
+      }
+   }
+  console.log('lite: '+lite+boolL);
+  
+   var trace1 = {
+    x: datax,
+    y: lite,
+    name: "LiteCoin Values",
+    type: "scatter"
+  };
+  var trace2 = {
+    x: datax,
+    y: boolL,
+    name: "Predictions",
+    type: "bar"
+  };
+  var data = [trace1, trace2];
+  var graphOptions = {filename: "LiteCoin", fileopt: "overwrite"};
+  plotly.plot(data, graphOptions, function (err, msg) {
+      console.log(msg);
+  });
+  
+  
+    });
+  });
+  
+  
+  
       
